44e06d053a35845436113d7431d0709d005e17d1
github shiory602 <suzushioufo@gmail.com>
2021-03-12 18:05:29 -0800

    add todays max-min tem

--- main.js
+++ main.js
@@ -7,58 +7,105 @@ let temp = document.querySelector(".current .temp");
 let date = document.querySelector(".current .date");
 let todaysWeather = document.getElementById("todays-weather");
 let weatherIcon = document.getElementById("weatherIcon");
+let highLow = document.getElementById("high-low");
 let weather1 = document.getElementById("weather1");
 let weather2 = document.getElementById("weather2");
 let weather3 = document.getElementById("weather3");
 let city = document.querySelector(".location .city");
 let country = document.querySelector(".location .country");
+let high = document.querySelector(".high");
+let low = document.querySelector(".low");
+let day1 = document.querySelector(".week-day .day1");
+let weekWeatherIcon1 = document.querySelector("#weekly-icon1");
+let dayWeather1 = document.querySelector(".week-day .day-weather1");
 
-// const input = document.querySelector('input[type="search"]');
 
+// // 曜日を表す文字列の配列を作っておく
+// var WeekChars = ["日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"];
 
+// // 日付オブジェクトから曜日を得る
+// var dObj = new Date( /* 何らかの日付 */ );
+// var wDay = dObj.getDay();
+// alert("指定の日は、" + WeekChars[wDay] + "です。");
+
+// 曜日と月の表示
 function dateBuilder(d) {
-	let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+	// let months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
 	let days = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
 
-	let day = days[d.getDay()]; // array[index]
-	let date = d.getDate();
-	let month = months[d.getMonth()];
-	let year = d.getFullYear();
+	let day = days[d.getDay() + 1]; // array[index]
+	// let date = d.getDate();
+	// let month = months[d.getMonth()];
+	// let year = d.getFullYear();
 
-	return `${day} ${date} ${month} ${year}`;
+	return `${day}`;
+	// return `${day} ${date} ${month} ${year}`;
 }
 
-// function changeBg(todaysWeather) {
-// 	console.log(todaysWeather);
-// 	switch (todaysWeather) {
-// 		case "Clouds":
-// 			document.body.style.backgroundImage = url('img/cloudy.jpg');
-// 			break;
-// 		case "Storm":
-// 			document.body.style.backgroundImage = url('./img/storm.jpg');
-// 			break;
-// 		case "Rainy":
-// 			document.body.style.backgroundImage = url('./img/rainy.jpg');
-// 			break;
-// 		default:
-// 			document.body.style.backgroundImage = url('./img/sunnybg.jpg');
-// 			break;
-// 	}
-// }
+//icon list: https://openweathermap.org/weather-conditions
+function changeBg(todaysWeather) {
+	switch (todaysWeather) {
+		case "01d":
+			document.body.style.backgroundImage = "url('./img/sunnybg.jpg')";
+			break;
+		case "02d":
+			document.body.style.backgroundImage = "url('./img/cloudy.jpg')";
+			break;
+		case "03d":
+			document.body.style.backgroundImage = "url('./img/cloudy.jpg')";
+			break;
+		case "04d":
+			document.body.style.backgroundImage = "url('./img/cloudy.jpg')";
+			break;
+		case "10d":
+			document.body.style.backgroundImage = "url('./img/rainy.jpg')";
+			break;
+		case "50d":
+			document.body.style.backgroundImage = "url('./img/rainy.jpg')";
+			break;
+		case "13d":
+			document.body.style.backgroundImage = "url('./img/snow.jpg')";
+			break;
+		case "09d":
+			document.body.style.backgroundImage = "url('./img/mist.jpg')";
+			break;
+		default:
+			document.body.style.backgroundImage = "url('./img/sunnybg.jpg')";
+			break;
+	}
+}
 
 function fillData(query) {
+	// 背景
+	changeBg(query.weather[0].icon);
+
+	// 気温
 	temp.innerHTML = `${Math.round(query.main.temp)}<span>℃</span>`; //四捨五入
 
-	let now = new Date();
-	date.innerText = dateBuilder(now);
+	// 日付
+	var sec = query.dt; // get second
+	var date = new Date(sec * 1000);
+	var datestr = date.toLocaleDateString()
+	var timestr = date.toLocaleTimeString([], {
+		hour: '2-digit',
+		minute: '2-digit'
+	})
+	$(".date").html(`${datestr} ${timestr}`);
 
+
+	// 天気
 	todaysWeather.innerHTML = query.weather[0].main;
-	// changeBg(query.weather[0].main);
 
+	// アイコン
 	let icon = `http://openweathermap.org/img/w/${query.weather[0].icon}.png`;
 	weatherIcon.innerHTML = `<img src=${icon} width="100">`;
+
+	// 都市・国名
 	city.innerText = query.name;
 	country.innerText = query.sys.country;
+
+	high.innerText = Math.round(query.main.temp_max);
+	low.innerText = Math.round(query.main.temp_min);
 }
 
 
@@ -86,7 +133,7 @@ const getResults = (city) => {
 
 // API references: https://openweathermap.org/api/one-call-api
 const hourlyResults = (query) => {
-	fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${query.coord.lat}&lon=${query.coord.lon}&exclude=current,daily,minutely,alert&appid=${api.key}`)
+	fetch(`https://api.openweathermap.org/data/2.5/onecall?lat=${query.coord.lat}&lon=${query.coord.lon}&date=${query.dt}&exclude=current,minutely,alert&units=metric&appid=${api.key}`)
 		.then(response => { // response = weather data
 			if (response.status !== 200) {
 				console.log(`Oops! we have an error ${response.status}`);
@@ -94,18 +141,53 @@ const hourlyResults = (query) => {
 			return response.json()
 		})
 		.then((query) => {
+
 			console.log(query);
-			// for (let i = 4; i < query.hourly; i++)
-			var sec = query.hourly[7].dt;
+
+			// weekly -----------------------------------
+
+			console.log(query.daily[0].dt);
+
+			// 曜日
+			let now = new Date();
+			day1.innerText = dateBuilder(now);
+
+			// アイコン
+			let icon = `http://openweathermap.org/img/w/${query.daily[1].weather[0].icon}.png`;
+			weekWeatherIcon1.innerHTML = `<img src=${icon} width="50">`;
+
+			// 気温
+			dayWeather1.innerHTML = `${Math.round(query.daily[1].temp.day)}<span>℃</span>`;
+
+
+
+
+			// hourly -------------------------------------------
+
+			var sec = query.hourly[7].dt; // get second
 			var date = new Date(sec * 1000);
-			var timestr = date.toLocaleTimeString([], { hour: '2-digit', minute: '2-digit' });
-
-			console.log(date, timestr);
-
+			var timestr = date.toLocaleTimeString([], {
+				hour: '2-digit',
+				minute: '2-digit'
+			});
 			$(".weather1").html(`${timestr}<span>${query.hourly[7].weather[0].main}</span>`);
-			$(".weather2").html(timestr);
 
-			// weather1.innerHTML = `<span>${query.}</span>`
+			var sec = query.hourly[14].dt; // get second
+			var date = new Date(sec * 1000);
+			var timestr = date.toLocaleTimeString([], {
+				hour: '2-digit',
+				minute: '2-digit'
+			})
+			$(".weather2").html(`${timestr}<span>${query.hourly[14].weather[0].main}</span>`);
+
+			var sec = query.hourly[21].dt; // get second
+			var date = new Date(sec * 1000);
+			var timestr = date.toLocaleTimeString([], {
+				hour: '2-digit',
+				minute: '2-digit'
+			})
+			// console.log(date, timestr);
+			$(".weather3").html(`${timestr}<span>${query.hourly[21].weather[0].main}</span>`);
 
 		})
 		.catch(error => {
@@ -115,17 +197,16 @@ const hourlyResults = (query) => {
 
 
 
-// const tomorrow = new Date(now)
-
-// weather1.innerHTML = `${day}<span>${tomorrow.setDate(tomorrow.getDate() + 1)}</span>`;
-// let weather2 = document.querySelector(".current .weather2");
-// let weather3 = document.querySelector(".current .weather3");
-
-
-let reloadDisplay = () => {
-	setTimeout(function () {
-		location.reload(); // reload every 2 mins
-	}, 120000);
+let reloadDisplay = (v) => {
+	if (v == "vancouver") {
+		setTimeout(function () {
+			location.reload(); // reload every 2 mins
+		}, 120000);
+	} else {
+		setTimeout(function () {
+			location.reload(); // reload every 2 mins
+		}, 120000);
+	}
 }
 
 let searchBox = document.getElementById("searchBox");
@@ -137,7 +218,8 @@ form.addEventListener('submit', (e) => {
 		// .appendChild(cityName);
 		getResults(cityName);
 		// hourlyResults(e.value);
-		searchBox.value = "Search for a city..." // clear the input
+		reloadDisplay(cityName);
+		searchBox.value = "" // clear the input
 	} else {
 		alert("it is empty");
 	}
@@ -146,4 +228,5 @@ form.addEventListener('submit', (e) => {
 $(document).ready(() => {
 	getResults("vancouver");
 	// hourlyResults();
+	reloadDisplay("vancouver");
 })
